366907bf31574e7afe96f818b59baf4cfd35ebf6
DABYCHO <ing.davidabraham@gmail.coM>
2025-07-17 21:09:21 -0600

    config: add webpack alias for src directory using @/ path

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -16,6 +18,13 @@ const nextConfig = {
         pathname: '**',
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    }
+    return config
   },
 }
 
